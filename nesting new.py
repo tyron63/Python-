aab41f8c3c188bd6a1f0dbb82152eda6cbabe3b9
Tyron keet <tyron.keet.01@gmail.com>
2020-03-17 16:26:33 +0200

    today

---
+++ nesting new.py
@@ -0,0 +1,11 @@
+for y in range(0,5,1):      #0 is where it starts, 5 is end and 1 is times table
+    for x in range(y):
+        print("*",end="")
+    print()
+for z in range(5,0,-1):      #5 is where it starts, 0 is end and -1 is times table
+    for w in range(z):
+         print("*",end="")
+    print ()
+
+
+
